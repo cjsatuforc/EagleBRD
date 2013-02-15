b5bacea2d759583965851aae8a53c6a3ed921835
riedmueller <riedmueller.f@gmail.com>
2013-02-15 14:36:09 +0100

    - Added more 3D models - Moved example to example folder - Added more Informations to README file

--- Init.py
+++ Init.py
@@ -1,9 +1,4 @@
-# Idf init module  
+# EagleBRD init module  
 #
-# Gathering all the information to start FreeCAD
-# This is the second one of three init scripts, the third one
-# runs when the gui is up
-
 
 FreeCAD.addImportType("EAGLE 6.x Board File Type (*.brd)","EagleBRD") 
-#FreeCAD.addImportType("IDF emp File Type (*.emp)","Import_Emp") 
